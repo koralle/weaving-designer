81ab577d12700785e9a772bdf3b3d48407ac1336
koralle <mkhd7930@gmail.com>
2023-03-01 00:50:01 +0900

    add: storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+        viteConfigPath: "./vite.config.ts",
+      },
+    },
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: (config) => {
+    return mergeConfig(config, {});
+  },
+};
+
+export default config;
