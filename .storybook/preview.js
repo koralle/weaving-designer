f7f330234dd637ae0bf6bea15a81a6909b98db6f
koralle <mkhd7930@gmail.com>
2023-03-07 22:59:29 +0900

    add preview.js for Storybook v7 + Vite + Tailwind CSS

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import "tailwindcss/tailwind.css";
+
+const parameters = {
+  actions: { argTypeRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export { parameters };
