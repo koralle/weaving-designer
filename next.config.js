ab24595271b17cdd5b7d958baf27d982f5960723
koralle <mkhd7930@gmail.com>
2023-03-19 12:09:11 +0900

    update: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
   images: {
     unoptimized: true,
   },
