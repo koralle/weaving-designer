6beb9107b99d784ddda12d6cc7da102c3309de37
koralle <mkhd7930@gmail.com>
2023-03-19 21:50:51 +0900

    update: config for Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
 import 'tailwindcss/tailwind.css';
+import * as nextImage from 'next/image';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => {
+    return <img {...props} />;
+  },
+});
 
 const parameters = {
   actions: { argTypeRegex: '^on[A-Z].*' },
@@ -7,6 +16,9 @@ const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 };
 
