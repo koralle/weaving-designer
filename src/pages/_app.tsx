34f8378e18f1309ea191329a87f76aebe0d6fec6
koralle <mkhd7930@gmail.com>
2023-03-25 01:00:16 +0900

    update: _app.tsx for Recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+
+import { Layout } from '../components/Layout';
 
 const TauriFrontendApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
+  );
 };
 
 export default TauriFrontendApp;
