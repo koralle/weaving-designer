500a0f6fb71e0935aa5ab1f7f654e30a8a1a6984
koralle <mkhd7930@gmail.com>
2023-03-25 01:00:32 +0900

    update: NextPage '/'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { SyntheticEvent } from 'react';
+
+import { Button } from '../components/Button';
 
 const Home: NextPage = () => {
-  return <h1>Next.js</h1>;
+  const router = useRouter();
+
+  const onClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(e);
+    await router.push('/draft-editor');
+  };
+
+  return (
+    <>
+      <Button
+        appearance='primary'
+        onClick={onClick}
+      >
+        新規作成
+      </Button>
+    </>
+  );
 };
 
 export default Home;
